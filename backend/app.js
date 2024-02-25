f2c0a2519352e706137ae37fe3aebd0ec038b01a
praveen kumar <praveenshakya1@gmail.com>
2024-02-25 16:55:19 +0530

    add preinstallation

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,17 @@ import { config } from "dotenv";
 import userRouter from "./routes/user.router.js";
 import authRouter from "./routes/auth.router.js";
 import listRouter from  "./routes/list.router.js";
-
 import { errorMiddleware } from "./middlerwares/errorMiddleware.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+
 
 config({
     path:"backend/config/config.env",
 })
+
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -23,5 +27,11 @@ app.use("/api/v1/listing", listRouter); // list routes
 
 app.use(errorMiddleware);
 
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
 
 export default app;
